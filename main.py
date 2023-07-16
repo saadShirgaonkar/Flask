e353d654bff2f6b8b14187aa7a325559840a2477
saadShirgaonkar <saadshirgaonkar183@gmail.com>
2023-07-16 15:57:29 +0530

    Added a few more

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def helloWorld():
-  return 'Hello First Flask App'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
