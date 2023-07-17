122a981148ce4ba4ccdfd5de0f94fb9d76a8260c
saadShirgaonkar <saadshirgaonkar183@gmail.com>
2023-07-17 16:06:30 +0530

    Flask Conn. Ready Some Changes

--- main.py
+++ main.py
@@ -27,31 +27,6 @@ conn.commit()
 cur.close()
 conn.close()
 app = Flask(__name__)
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': 'Rs. 10,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Delhi, India',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote'
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco, USA',
-        'salary': '$150,000'
-    }
-]
 
 
 @app.route('/')
